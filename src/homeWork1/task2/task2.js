6223d8bec9df47eada9f28022849d834c1100377
Yurii_Bobeshko <Yurii_Bobeshko@epam.com>
2022-05-22 19:01:36 +0300

    set up

--- src/homeWork1/task2/task2.js
+++
@@ -1,9 +0,0 @@
-import fullFileWriter from './fullFileWriter/fullFileWriter'
-import byLineFileWriter from './byLineFileWriter/byLineFileWriter.js'
-import path from 'path'
-
-const csvFilePath = path.join(__dirname, 'csv/nodejs-hw1-ex1.csv')
-const txtFilePath = path.join( __dirname, '/txt/file.txt')
-
-fullFileWriter(csvFilePath, txtFilePath)
-byLineFileWriter(csvFilePath, txtFilePath)
--- src/homeWork1/task2/utils/writeText/writeText.js
+++
@@ -1,9 +0,0 @@
-import fs from "fs";
-
-export default function writeText(text, txtFilePath) {
-    fs.appendFile(txtFilePath, text, err => {
-        if (err) {
-            console.error(err)
-        }
-    })
-}
